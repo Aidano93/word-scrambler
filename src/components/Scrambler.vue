0952c0ab37c6503475261f8da56383bd7d9275e4
Aidan O'Reilly <ajoreilly93@gmail.com>
2021-08-19 17:40:01 -0600

    step 2: successful word scrambler

--- src/components/Scrambler.vue
+++ src/components/Scrambler.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 id="scrambled-word">{{ sentence }}</h2>
+    <h2 id="scrambled-word">{{ scrambledSentence }}</h2>
   </div>
 </template>
 <script>
@@ -12,20 +12,41 @@ export default {
     return {
       fetchCounter: 1,
       data: {},
-      sentence: this.data.sentence
+      sentence: "",
     };
   },
   computed: {
     scrambledSentence() {
       const senArr = this.sentence.split(" ");
-      return {}
-    }
+      let scrArr = [];
+      senArr.forEach((word) => {
+        if (word.length <= 2) {
+          scrArr.push(word);
+        } else {
+          const splitArr = word.split("");
+          const sliceArr = splitArr.slice(1, splitArr.length - 1);
+          const n = sliceArr.length;
+          for (let i = n - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const tmp = sliceArr[i];
+            sliceArr[i] = sliceArr[j];
+            sliceArr[j] = tmp;
+          }
+          sliceArr.unshift(splitArr[0]);
+          sliceArr.push(splitArr[splitArr.length - 1]);
+          scrArr.push(sliceArr.join(""));
+        }
+      });
+
+      return scrArr.join(" ");
+    },
   },
   async mounted() {
     let url = `https://api.hatchways.io/assessment/sentences/${this.fetchCounter}`;
     try {
       const res = await axios.get(url);
       this.data = res.data.data;
+      this.sentence = res.data.data.sentence;
     } catch (err) {
       console.error(err);
     }
